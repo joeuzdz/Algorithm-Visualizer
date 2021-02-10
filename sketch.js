f6c4be447a6589b1d543e733ece685406a709507
BioticComponent <juzdzinski@gmail.com>
2021-02-10 14:00:18 -0500

    display title and screen dimensions

--- sketch.js
+++ sketch.js
@@ -1,16 +1,48 @@
-let x = 0;
+let backgroundColor;
+let font;
 
 function setup() {
-    createCanvas(400, 400);
-    
+    createCanvas(windowWidth, windowHeight);
+    backgroundColor = color(150);
+    font = 'monospace';
+    textFont(font);
 }
 
 function draw() {
-    background(50);
+    resizeCanvas(windowWidth, windowHeight);
+    background(backgroundColor);
+    //displayGridLines();
+    displayTitle();
+    displayScreenDimensions();
+}
 
-    ellipse(x, height/2, 50);
-    x += 10;
-    if (x > width) {
-        x = 0;
+function displayTitle() {
+    push();
+    fill(50);
+    noStroke();
+    textAlign(CENTER, TOP);
+    if (width > 1000) {
+        textSize(64);
+    } else {
+        textSize(map(width, 0, 1000, 16, 64));
     }
+    text('Algorithm Visualizer', width/2, height*0.02);
+    pop();
+}
+
+function displayScreenDimensions() {
+    push();
+    textSize(16);
+    let string = str(width) + ', ' + str(height);
+    text(string, width - 100, height - 20);
+    pop();
+}
+
+function displayGridLines() {
+    push();
+    stroke(0);
+    strokeWeight(0.2);
+    line(0, height/2, width, height/2);
+    line(width/2, 0, width/2, height);
+    pop();
 }
